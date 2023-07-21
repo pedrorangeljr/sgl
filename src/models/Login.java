f319dbf7c59dec7ed099c370983168d2589e7a1f
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-07-20 23:17:40 -0300

    criação da classe Login

---
+++ src/models/Login.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.io.Serializable;
+
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String email;
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
