10a8bfa6bc8d0aa91e3b1425a2d023a9f334604c
pedrorengeljr <pedro.curtis.rangel@gmail.com>
2023-08-24 10:33:06 -0300

    Criação dos atributos classe Livros

--- src/models/Livros.java
+++ src/models/Livros.java
@@ -2,4 +2,59 @@ package models;
 
 public class Livros {
 
+	private Long id;
+	private String titulo;
+	private String autor;
+	private String categoria;
+	private String email;
+	private int quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
 }
